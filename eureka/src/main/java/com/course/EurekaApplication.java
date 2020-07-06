cac5f52b99e498afac37471a8c2873ccc84a3a2d
louyilin <louyilin1996@gmail.com>
2020-07-06 19:52:36 -0400

    7.6
    1. Add Eureka as register service
    2. Change Maven to Parent-Child

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.muke.springcloudvue;
+package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudVueApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringcloudVueApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
