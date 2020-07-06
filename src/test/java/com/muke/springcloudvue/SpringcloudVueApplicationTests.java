6873f0574c3b548dcd9d1ed8eca28851e3c23e13
louyilin <louyilin1996@gmail.com>
2020-07-06 19:20:55 -0400

    first commit

---
+++ src/test/java/com/muke/springcloudvue/SpringcloudVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muke.springcloudvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
