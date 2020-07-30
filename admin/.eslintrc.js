e7cd8e4cfc114be67db3d1d581e0fffd36202723
louyilin <louyilin1996@gmail.com>
2020-07-29 20:59:08 -0400

    简化登陆界面 增加eslintrc.js解决“$”报错

---
+++ admin/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-undef': 'off',
+        'vue/no-unused-vars': 'off',
+        'vue/require-v-for-key': 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+};
