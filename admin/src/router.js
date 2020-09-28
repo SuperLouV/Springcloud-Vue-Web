546885c4c41f408e4a2e7c30796e8be598fe090a
louyilin <louyilin1996@gmail.com>
2020-09-28 00:51:51 -0400

    add admin.vue

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login.vue'
+import Admin from './views/admin.vue'
 
 Vue.use(Router);
 
@@ -8,10 +9,13 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
-        path: '*',      //all paths
+        path: '*',
         redirect: "/login",
     }, {
         path: '/login',
         component: Login
+    }, {
+        path: '/admin',
+        component: Admin
     }]
 })
