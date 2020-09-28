3ad92926e1199151fee0ea468526868d6a0a88ac
louyilin <louyilin1996@gmail.com>
2020-09-28 00:55:15 -0400

    login redirect to admin

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -27,28 +27,30 @@
                                         <form>
                                             <fieldset>
                                                 <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
-															<i class="ace-icon fa fa-user"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input type="text" class="form-control" placeholder="Username"/>
+                            <i class="ace-icon fa fa-user"></i>
+                          </span>
                                                 </label>
 
                                                 <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input type="password" class="form-control" placeholder="Password"/>
+                            <i class="ace-icon fa fa-lock"></i>
+                          </span>
                                                 </label>
 
                                                 <div class="space"></div>
 
                                                 <div class="clearfix">
                                                     <label class="inline">
-                                                        <input type="checkbox" class="ace" />
+                                                        <input type="checkbox" class="ace"/>
                                                         <span class="lbl">记住我</span>
                                                     </label>
 
-                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                                                    <button type="button"
+                                                            class="width-35 pull-right btn btn-sm btn-primary"
+                                                            v-on:click="login()">
                                                         <i class="ace-icon fa fa-key"></i>
                                                         <span class="bigger-110">登录</span>
                                                     </button>
@@ -74,6 +76,11 @@
 <script>
     $('body').attr('class', 'login-layout light-login');
     export default {
-        name: 'app',
+        name: 'login',
+        methods: {
+            login () {
+                this.$router.push("/admin")
+            }
+        }
     }
 </script>
