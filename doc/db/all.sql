23906e12cf6c1f8294996cb5536b111062a402c5
louyilin <louyilin1996@gmail.com>
2020-11-20 22:31:37 -0500

    use generator to build "chapter"

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,9 +1,19 @@
+drop table if exists `chapter`;
+create table `chapter` (
+                           `id` char(8) not null comment 'ID',
+                           `course_id` char(8) comment '课程ID',
+                           `name` varchar(50) comment '名称',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
+
+
+
 drop table if exists `test`;
 create table `test` (
                         `id` char(8) not null default '' comment 'id',
                         `name` varchar(50) comment '名称',
                         primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='test';
+) engine=innodb default charset=utf8mb4 comment='测试';
 
-insert into `test` (id, name) values (1, 'test');
-insert into `test` (id, name) values (2, 'test2');
+insert into `test` (id, name) values (1, '测试');
+insert into `test` (id, name) values (2, '测试2');
