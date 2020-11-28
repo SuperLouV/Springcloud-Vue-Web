42d721a10df1ddb4e349fb4f84d7bf3d9b5bd825
louyilin <louyilin1996@gmail.com>
2020-11-28 16:08:00 -0500

    generator dto

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -6,23 +6,53 @@ package com.course.generator.server;
  * @Email:ylou7@stevens.edu
  */
 
+import com.course.generator.util.Field;
+import com.course.generator.util.DbUtil;
 import com.course.generator.util.FreemarkerUtil;
-import freemarker.template.TemplateException;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ServerGenerator {
+    static String MODULE = "business";
     static String toServicePath = "server/src/main/java/com/course/server/service/";
-    public static void main(String[] args) throws IOException, TemplateException {
+    static String toControllerPath = MODULE+"/src/main/java/com/course/"+MODULE+"/controller/admin/";
+    static String toDtoPath = "server/src/main/java/com/course/server/dto/";
+    public static void main(String[] args) throws Exception {
         String Domain = "Section";
         String domain = "section";
+        String tableNameCn = "小节";
+        String module = MODULE;
+        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+        Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
         map.put("domain", domain);
+        map.put("tableNameCn", tableNameCn);
+        map.put("module", module);
+        map.put("fieldList", fieldList);
+        map.put("typeSet", typeSet);
 
+        // 生成dto
+        FreemarkerUtil.initConfig("dto.ftl");
+        FreemarkerUtil.generator(toDtoPath + Domain + "Dto.java", map);
+
+        // 生成service
         FreemarkerUtil.initConfig("service.ftl");
         FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
+
+        // 生成controller
+        FreemarkerUtil.initConfig("controller.ftl");
+        FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java", map);
+    }
+
+    /**
+     * 获取所有的Java类型，使用Set去重
+     */
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
