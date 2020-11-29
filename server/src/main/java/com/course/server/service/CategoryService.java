a1b413c47252aeb131ae1a543da5d765ebd15f35
louyilin <louyilin1996@gmail.com>
2020-11-29 17:41:24 -0500

    make categories as tree structure

--- server/src/main/java/com/course/server/service/CategoryService.java
+++ server/src/main/java/com/course/server/service/CategoryService.java
@@ -10,6 +10,7 @@ import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -42,8 +43,7 @@ public class CategoryService {
         List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
         PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
         pageDto.setTotal(pageInfo.getTotal());
-        List
-                <CategoryDto> categoryDtoList = CopyUtil.copyList(categoryList, CategoryDto.class);
+        List<CategoryDto> categoryDtoList = CopyUtil.copyList(categoryList, CategoryDto.class);
         pageDto.setList(categoryDtoList);
     }
 
@@ -77,8 +77,23 @@ public class CategoryService {
     /**
      * 删除
      */
+    @Transactional
     public void delete(String id) {
+        deleteChildren(id);
         categoryMapper.deleteByPrimaryKey(id);
     }
-}
 
+    /**
+     * 删除子分类
+     * @param id
+     */
+    public void deleteChildren(String id) {
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        if ("00000000".equals(category.getParent())) {
+            // 如果是一级分类，需要删除其下的二级分类
+            CategoryExample example = new CategoryExample();
+            example.createCriteria().andParentEqualTo(category.getId());
+            categoryMapper.deleteByExample(example);
+        }
+    }
+}
